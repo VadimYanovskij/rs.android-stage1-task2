1922f9eadfa0062e12cac2313fdc0814458bffc2
VadimYanovskij <61162599+VadimYanovskij@users.noreply.github.com>
2020-04-10 23:07:17 +0300

    Add: Task2.subtask3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,16 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+    //    throw NotImplementedError("Not implemented")
+        var rez=""
+        var result = "NO"
+        for (itemA in a)
+            for (itemB in b) {
+                if (itemA==itemB || itemA==itemB+32 ) rez += itemA
+            }
+        if (b.indices==rez.indices)
+            for (i in b.indices)
+                if (b[i]==rez[i] || b[i]+32==rez[i]) result = "YES"
+    return result
     }
 }
--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -7,9 +7,9 @@ import kotlin.reflect.KClass
 class Blocks {
 
     // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
      //   throw NotImplementedError("Not implemented")
-        val array = blockA.toMutableList().filter { it::class == blockB }
+        val array = blockA.toMutableList().filter { it!!::class == blockB }
         return when (blockB) {
             String::class -> array.joinToString("")
             Int::class -> array.sumBy { it.toString().toInt() }
