74e4d9e034e93094107be93e633783212bd78237
VadimYanovskij <myemail11@gmail.com>
2020-04-10 16:06:45 +0300

    refactor: subtask2 and subtask5

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -6,29 +6,19 @@ class TimeConverter {
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
   //      throw NotImplementedError("Not implemented")
-        var result: String = ""
-        when(minute.toInt()){
-            0 -> result = numWrite(hour) + " o' clock"
-            15 -> result = "quarter past " + numWrite(hour)
-            in 1..29 -> result = numWrite(minute) + " minutes past " + numWrite(hour)
-            30 -> result = "half past " + numWrite(hour)
-            45 -> {
-                val hour2 = hour.toInt()+1
-                result = "quarter to " + numWrite(hour2.toString())
-            }
-            in 31..59 -> {
-                val hour2 = hour.toInt()+1
-                val minute2 = 60 - minute.toInt()
-                result = numWrite(minute2.toString()) + " minutes to " + numWrite(hour2.toString())
-            }
-
+        return when(minute.toInt()){
+            0 -> numWrite(hour) + " o' clock"
+            15 -> "quarter past " + numWrite(hour)
+            in 1..29 -> numWrite(minute) + " minutes past " + numWrite(hour)
+            30 -> "half past " + numWrite(hour)
+            45 -> "quarter to " + numWrite((hour.toInt()+1).toString())
+            in 31..59 -> numWrite((60-minute.toInt()).toString()) + " minutes to " + numWrite((hour.toInt()+1).toString())
+            else -> ""
         }
 
-        return result
     }
 
     fun numWrite (num: String): String{
-        var str: String = ""
         val numbers = arrayOf("one", "two", "three", "four", "five",
                                             "six", "seven", "eight", "nine", "ten",
                                             "eleven", "twelve", "thirteen", "fourteen", "fifteen",
@@ -36,7 +26,6 @@ class TimeConverter {
                                             "twenty one", "twenty two", "twenty three", "twenty four",
                                             "twenty five", "twenty six", "twenty seven", "twenty eight",
                                             "twenty nine")
-        str = numbers[num.toInt()-1]
-        return  str
+        return numbers[num.toInt()-1]
     }
 }
--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,21 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+     //   throw NotImplementedError("Not implemented")
+        val array = blockA.toMutableList().filter { it::class == blockB }
+        return when (blockB) {
+            String::class -> array.joinToString("")
+            Int::class -> array.sumBy { it.toString().toInt() }
+            LocalDate::class -> array.stream().map{ it as LocalDate }.max(LocalDate::compareTo).get().format(
+                DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            else -> ""
+        }
     }
 }
