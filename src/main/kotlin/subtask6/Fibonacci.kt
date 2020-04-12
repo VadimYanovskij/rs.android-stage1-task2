21a00454ebfaaf98f7b5659508cfdcf34c8fdf01
VadimYanovskij <61162599+VadimYanovskij@users.noreply.github.com>
2020-04-12 21:51:15 +0300

    Add: Task2.subtask6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,16 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+    //    throw NotImplementedError("Not implemented")
+        var fib0 = 0
+        var fib1 = 1
+        var fib2 = fib0 + fib1
+        while ( fib1 * fib2 < n ) {
+            fib0 = fib1
+            fib1 = fib2
+            fib2 = fib0 + fib1
+        }
+        return if ( fib1 * fib2 == n ) intArrayOf ( fib1, fib2, 1 )
+                  else intArrayOf( fib1, fib2, 0 )
     }
 }
